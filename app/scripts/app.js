f5483518093f8cdd364ba8db300146e77a92b01c
Alex Johnson <alex@tablelist.com>
2015-08-07 17:48:47 -0400

    Setup ui-router

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -18,32 +18,30 @@ angular
     'ngTouch',
     'ui.router',
   ])
-  .config(function ($routeProvider) {
-    $routeProvider
-      .when('/', {
-        templateUrl: 'views/main.html',
-        controller: 'MainCtrl',
-        controllerAs: 'main'
-      })
-      .when('/recipes', {
-        templateUrl: 'views/recipes.html',
-        controller: 'RecipesCtrl',
-        controllerAs: 'recipes'
-      })
-      .when('/recipe/:slug/:step', {
-        templateUrl: 'views/recipe.html',
-        controller: 'RecipeCtrl',
-        controllerAs: 'recipe'
-      })
-      // 404
-      .otherwise({
-        redirectTo: '/'
-      });
-  })
-  .run(function($rootScope){
-
-    console.log("App Initialized & Running...");
-
-    $rootScope.user = null;
-
+  .config(function ($stateProvider, $urlRouterProvider) {
+    
+    $stateProvider
+    .state('home', {
+      url: "/",
+      templateUrl: 'views/main.html',
+      controller: 'MainCtrl',
+    })
+    .state('recipes', {
+      url: "/recipes",
+      templateUrl: "views/recipes.html",
+      controller: 'RecipesCtrl',
+    })
+    .state('recipe', {
+      url: "/recipe",
+      templateUrl: "views/recipe.html",
+      controller: 'RecipeCtrl',
     });
+    
+  });
+    
+    // .run(function($rootScope){
+
+    //   console.log("App Initialized & Running...");
+    //   $rootScope.user = null;
+
+    // })
